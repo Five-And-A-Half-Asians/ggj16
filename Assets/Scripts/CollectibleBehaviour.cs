fe3231262faf4ba3faa4e9c9429ea3cbe6156d17
Paul <paul_wang347@hotmail.com>
2016-01-29 23:57:39 -0800

    Updated skybox, collectible script, partial MainScene script

---
+++ Assets/Scripts/CollectibleBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleBehaviour : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == player) {
+            player.GetComponent<ScoreManager>().addCollectible("C1");
+            //Destroy(gameObject);
+            FindObjectOfType<MainScene>().checkKeyPointCollision(gameObject.GetInstanceID());
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/MainScene.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainScene : MonoBehaviour {
+    
+    public float min_rand = 0f;
+    public float max_rand = 100f;
+
+    List<int> keypointIDs;
+    int index = 0;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool CheckKeyPointCollision(int ID)
+    {
+        if(keypointIDs[index] == ID)
+        {
+            index++;
+            return true;
+        } else
+        {
+            //game over
+            return false;
+        }
+    }
+
+    void SpawnKeypoints()
+    {
+        float x = Random.Range(min_rand, max_rand);
+        float y = Random.Range(min_rand, max_rand);
+        float z = Random.Range(min_rand, max_rand);
+        Vector3 spawn = new Vector3(x, y, z);
+
+
+    }
+}
